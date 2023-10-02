5d1f8806c10edf82e47fa28d979e8ab5f97cb028
jeyaseelan <115181970+JEYASEELAN-2399@users.noreply.github.com>
2023-10-02 10:35:13 +0000

    added h2 element

--- src/Head.js
+++ src/Head.js
@@ -1,12 +1,18 @@
 export default function Jeii(){
 
 return (
-
+<>
     <div>
         <h1>hiiii jeyaseelan</h1>
+
     </div>
+  <div>
+    <h2>
+        welcome to react 
+    </h2>
+  </div>
 
-
+</>
 )
 
 };
