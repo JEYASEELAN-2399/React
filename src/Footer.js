61e98ac049ee404227c74ce282a50f47151071b4
jeyaseelan <115181970+JEYASEELAN-2399@users.noreply.github.com>
2023-10-03 05:40:23 +0000

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,8 @@
 export default function Foot() {
-  return <h6>hey look this is in h6 </h6>;
+  return (
+    <>
+      <h6>hey look this is in h6 </h6>
+      <h5>this is h5</h5>
+    </>
+  );
 }
-
-
