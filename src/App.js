d0d2d744d8a73a82097bd1d1d43600ce77a11a39
JEYASEELAN-2399 <115181970+JEYASEELAN-2399@users.noreply.github.com>
2023-10-03 11:01:27 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ export default function App() {
         <h2>Start editing to see some magic happen!</h2>
         <Jeii />
         <Foot />
-        <Foot_1 />
       </div>
     </>
   );
--- src/Footer.js
+++ src/Footer.js
@@ -2,6 +2,4 @@ export default function Foot() {
   return <h6>hey look this is in h6 </h6>;
 }
 
-export function Foot_1() {
-  return <h5>this is in h5 format</h5>;
-}
+
