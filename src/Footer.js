11463daa0817b6bccff2fe666ec9a5b4f7df7981
JEYASEELAN-2399 <115181970+JEYASEELAN-2399@users.noreply.github.com>
2023-10-03 11:35:27 +0530

    added in vs code

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ export default function Foot() {
   return (
     <>
       <h6>hey look this is in h6 </h6>
- 
+<h3>iam working in vscode</h3> 
       <p>i have removed the above thing </p>
       <p>and added same h3 element below</p>
       <h3>efefefefefwefehth</h3>
