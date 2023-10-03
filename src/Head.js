22a62b7db8685c2129103bd5b5123662a7b55829
jeyaseelan <115181970+JEYASEELAN-2399@users.noreply.github.com>
2023-10-03 05:18:47 +0000

    this is branch 2

--- src/Head.js
+++ src/Head.js
@@ -1,18 +1,13 @@
-export default function Jeii(){
-
-return (
-<>
-    <div>
+export default function Jeii() {
+  return (
+    <>
+      <div>
         <h1>hiiii jeyaseelan</h1>
-
-    </div>
-  <div>
-    <h2>
-        welcome to react 
-    </h2>
-  </div>
-
-</>
-)
-
-};
+        <h2>this is branch 2</h2>
+      </div>
+      <div>
+        <h2>welcome to react</h2>
+      </div>
+    </>
+  );
+}
