1f84b6d811710185335d881b69ed503f06535504
JEYASEELAN-2399 <115181970+JEYASEELAN-2399@users.noreply.github.com>
2023-10-03 11:28:31 +0530

    removed and added

--- src/Footer.js
+++ src/Footer.js
@@ -3,6 +3,9 @@ export default function Foot() {
     <>
       <h6>hey look this is in h6 </h6>
       <h3>rfgrrf</h3>
+      <p>i have removed the above thing </p>
+      <p>and added same h3 element below</p>
+      <h3>efefefefefwefehth</h3>
     </>
   );
 }
